686372a152654bc3399c08e4b58fd21d95baf9c7
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-09-02 15:48:45 +0200

    feat(JPA) : create JPA files for Adventurer and Assignment, leave Quest empty for Hugo to complete

---
+++ src/main/java/hugonelson/guildboard/entity/Assignment.java
@@ -0,0 +1,34 @@
+package hugonelson.guildboard.entity;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // annotation saying it's a JPA entity 
+public class Assignment {
+
+    @Id // indicates a primary key 
+    @GeneratedValue(strategy=GenerationType.AUTO) // auto-increment 
+    private int id; 
+
+    // foreign keys
+    private int adventurer;
+    private int quest; 
+
+    private LocalDateTime assignedAt;
+    private LocalDateTime completedAt; 
+
+    // Empty constructor that only exists for the sake of JPA 
+    protected Assignment() {}; 
+
+    // Actual constructor to create persistent objects in database 
+    public Assignment(int adventurer, int quest, LocalDateTime assignedAt) {
+        this.adventurer = adventurer; 
+        this.quest = quest; 
+        this.assignedAt = assignedAt; 
+        this.completedAt = null; 
+    }
+
+}
---
+++ src/main/java/hugonelson/guildboard/entity/Quest.java
@@ -0,0 +1,5 @@
+package hugonelson.guildboard.entity;
+
+public class Quest {
+
+}
