5352cb7a11d4aed0a82b6ae48df3400cda2615e7
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-09-03 09:33:23 +0200

    test : add short sql seed to test get all adventurers route

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- This file will always seed the database on app launch
+
+INSERT INTO adventurer (name, character_class, level, gold, xp) VALUES
+('Aragorn', 'WARRIOR', 1, 0, 0),
+('Gandalf', 'MAGE', 1, 0, 0);
