d33cb4259d88294ad9fa60dd0ebc0111ef4c9f2e
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-09-04 10:17:08 +0200

    fix : change complete() signature and how it fetches all 3 entities to match API route parameters (only quest ID)

--- backend/src/main/java/hugonelson/guildboard/service/AssignmentService.java
+++ backend/src/main/java/hugonelson/guildboard/service/AssignmentService.java
@@ -81,9 +81,9 @@ public class AssignmentService {
     }
 
     // complete a quest, reward gold and xp, run the RG3 level loop 
-    public void complete(Long questId, Long adventurerId, Long assignmentId) {
-
-        Optional<Assignment> maybeAssigment = assignmentRepository.findById(assignmentId);
+    public void complete(Long questId) {
+        
+        Optional<Assignment> maybeAssignment = assignmentRepository.findByQuest_Id(questId);;
         Assignment assignment;
         if (maybeAssignment.isPresent()) {
             assignment = maybeAssignment.get();
@@ -92,23 +92,8 @@ public class AssignmentService {
             throw new ApiException(HttpStatus.NOT_FOUND, "ASSIGNMENT_NOT_FOUND", "Cannot find the assignment."); // 404 
         }
 
-        Optional<Adventurer> maybeAdventurer = adventurerRepository.findById(adventurerId);
-        Adventurer adventurer;
-        if (maybeAdventurer.isPresent()) {
-            adventurer = maybeAdventurer.get();
-        } 
-        else {
-            throw new ApiException(HttpStatus.NOT_FOUND, "ADVENTURER_NOT_FOUND", "Cannot find the adventurer."); // 404 
-        }
-
-        Optional<Quest> maybeQuest = questRepository.findById(questId);
-        Quest quest;
-        if (maybeQuest.isPresent()) {
-            quest = maybeQuest.get();
-        } 
-        else {
-            throw new ApiException(HttpStatus.NOT_FOUND, "QUEST_NOT_FOUND", "Cannot find the quest."); // 404 
-        }
+        Adventurer adventurer = assignment.getAdventurer();
+        Quest quest = assignment.getQuest(); 
 
         // award Gold and Xp
         adventurer.setGold(adventurer.getGold() + quest.getGoldReward()); 
