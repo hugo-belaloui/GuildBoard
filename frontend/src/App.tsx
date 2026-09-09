8e25bb36696e0faceae2a6fb9c2cfa89239bf268
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-09-09 12:34:38 +0200

    feat : establish home page skeletton + try out tailwind syntax and features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
 function App() {
 
   return (
-    <h1>Hello, World!</h1>
+    <div>
+      <nav className="flex items-center justify-between bg-blue-100 text-white p-4">
+        <h1 className="text-blue-900 font-bold">Guildboard</h1>
+        <div className="flex gap-2">
+          <button className="text-blue-900 font-semibold">Home</button>
+          <button className="text-blue-900 font-semibold">Adventurers</button>
+          <button className="text-blue-900 font-semibold">Quests</button>
+        </div>
+      </nav>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-4">
+        <section className="bg-blue-100 text-blue-900 p-4 rounded-lg">
+          <h2 className="font-semibold">Quests</h2>
+          <button className="font-bold">BROWSE ALL ADVENTURERS</button>
+          <button className="font-bold">CREATE NEW ADVENTURER</button>
+        </section>
+        <section className="bg-blue-100 text-blue-900 font-semibold p-4 rounded-lg">
+          <h2 className="font-semibold">Adventurers</h2>
+          <button className="font-bold">BROWSE ALL ADVENTURERS</button>
+          <button className="font-bold">CREATE NEW ADVENTURER</button>
+        </section>
+      </div>
+  </div>
   )
 
 }
