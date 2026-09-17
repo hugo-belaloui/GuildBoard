fadf25258c6fea76fe941345024b4fa423b211ae
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-09-17 11:02:03 +0200

    fix(hooks) : add refresh index and refetch method to useQuest

--- frontend/src/hooks/useQuest.ts
+++ frontend/src/hooks/useQuest.ts
@@ -10,6 +10,10 @@ export function useQuest(id : number | undefined) {
     const [quest, setQuest] = useState<Quest | null>(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<ApiError | null>(null);
+    // just a counter added to the dependancy array 
+    // when incremented, simply reloads the effect
+    // used to refresh on "Complete Quest"
+    const [refetchIndex, setRefetchIndex] = useState(0);
 
     useEffect(() => {
         if (id === undefined) {
@@ -22,8 +26,12 @@ export function useQuest(id : number | undefined) {
             .catch(setError)
             .finally(() => setIsLoading(false));
     // add id to the dependencies array, so it refreshes if the id changes 
-    }, [id]);
+    }, [id, refetchIndex]);
 
-    return { quest, isLoading, error };
+    function refetch() {
+        setRefetchIndex((prev) => prev + 1);
+    }
+
+    return { quest, isLoading, error, refetch };
 }
 
